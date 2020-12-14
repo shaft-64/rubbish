99670a98acc5c68e68e384de0ef3f8f3923fc103
eyewis <i@thac.cc>
2020-12-14 09:55:43 +0800

    1.0

--- Snape-rm/js/sanpe-diy.js
+++ Snape-rm/js/sanpe-diy.js
@@ -1,16 +1,14 @@
-
-
 $(document).ready(function(){
     $(".page-sidebar #sidebar").affix({
         offset: {
             top:0,
-            bottom:218
+            bottom:170
      	}
     });
     $(".article-sidebar #sidebar").affix({
         offset: {
             top:200,
-            bottom:208
+            bottom:164
      	}
     });
 });
@@ -18,9 +16,21 @@ $(document).ready(function(){
 let top_search=document.getElementById("search-panel");
 let searchIcon=document.getElementById("a-search");
 let closeIcon=document.getElementById("a-close");
+let switchs=document.getElementById("a-mode");
+let sm=document.getElementById("sun-moon");
 searchIcon.addEventListener("click",()=> {
     top_search.className="top-search";
 });
 closeIcon.addEventListener("click",()=>{
     top_search.className="top-search-hidden";
 });
+switchs.addEventListener("click",()=>{
+
+    if(sm.getAttribute("class")=="fa fa-moon"){
+        console.log("......")
+        sm.classList("fa","fa-sun")
+    }
+    if(sm.getAttribute("class")=="fa fa-sun"){
+        sm.className="fa fa-moon";
+    }
+})
